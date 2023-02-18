82066ee1976b5a85eacdb47a8fcb5762bc2f8215
nazmee46 <124545775+nazmee46@users.noreply.github.com>
2023-02-18 08:05:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port_number = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   const client = new Client({
-  connectionString: 'postgres://pasbivpbrzrvtt:3ca4c3f0167b8137b7bf38383ee29075f8df3b5c7e87a6cfc1859391d7ff89bd@ec2-34-194-73-236.compute-1.amazonaws.com:5432/dh6jb4b8j0cp6',
+  connectionString: 'postgres://lmhrwfibwopckp:67fb2c92cbb0266092ddcbb3a8743609bf7194658569cece25302d1e17ea652f@ec2-34-194-158-176.compute-1.amazonaws.com:5432/df2vgfq54gceej',
   ssl: {
     rejectUnauthorized: false
   }
